d10efdd95ea461728a1e9f4fe904672c5298b33a
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-04 15:27:20 +0300

    Implement shared modules.

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       exposes: {
         "./CartValue": "./src/index",
       },
+      shared: ["faker"],
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
